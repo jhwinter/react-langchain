72951676c2080fd05c19554e802588af18137fda
jhwinter <14960780+jhwinter@users.noreply.github.com>
2025-03-06 00:08:55 -0500

    replace openai with google

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from typing import Union, List
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.schema import AgentAction, AgentFinish
 from langchain.tools import Tool, tool
@@ -36,9 +37,9 @@ if __name__ == "__main__":
     Answer the following questions as best you can. You have access to the following tools:
 
     {tools}
-    
+
     Use the following format:
-    
+
     Question: the input question you must answer
     Thought: you should always think about what to do
     Action: the action to take, should be one of [{tool_names}]
@@ -47,9 +48,9 @@ if __name__ == "__main__":
     ... (this Thought/Action/Action Input/Observation can repeat N times)
     Thought: I now know the final answer
     Final Answer: the final answer to the original input question
-    
+
     Begin!
-    
+
     Question: {input}
     Thought:
     """
@@ -59,4 +60,5 @@ if __name__ == "__main__":
         tool_names=", ".join([t.name for t in tools]),
     )
 
-    llm = ChatOpenAI(temperature=0, stop = ["\nObservation", "Observation"])
+    llm = ChatGoogleGenerativeAI(temperature=0, model=os.environ["MODEL_NAME"])
+
